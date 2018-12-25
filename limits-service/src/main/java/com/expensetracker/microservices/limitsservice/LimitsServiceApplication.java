970544fb7215866a8e983ad4f986dfabceef6c67
akshayasridharan1 <46118859+akshayasridharan1@users.noreply.github.com>
2018-12-25 23:53:43 +1100

    Connect config-client to Spring-cloud-config-server
    Added ev,qa,defualt profiles for the config-client (limits service).
    
    No need to restart the server to get the properties refreshed, instead use GIT in config-server and @RefreshScope in client to refresh the git repository.
    
    Refresh is enabled using http://localhost:8981/actuator/refresh
    
    This refreshes/updates  the git values to the config client without restarting the server.

--- limits-service/src/main/java/com/expensetracker/microservices/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/expensetracker/microservices/limitsservice/LimitsServiceApplication.java
@@ -2,6 +2,8 @@ package com.expensetracker.microservices.limitsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
 
 @SpringBootApplication
 public class LimitsServiceApplication {
